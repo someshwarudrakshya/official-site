d9640101755e3a4eb58a7fddc5060e692d5a425e
someshwarudrakshya <someshwarudrakshya@gmail.com>
2025-09-09 07:52:02 +0545

    up

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -33,7 +33,7 @@ export default function Career() {
   };
 
   return (
-    <div className="container">
+    <section className="container">
       <div className="section-title">
         <h2>
           Join <span className="highlight">Our Team</span>
@@ -94,6 +94,6 @@ export default function Career() {
       </div>
 
       <Note type="Career" />
-    </div>
+    </section>
   );
 }
